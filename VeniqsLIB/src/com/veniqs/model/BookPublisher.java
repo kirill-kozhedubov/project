3afe35416eab3185331a463b3ed2d2ee5cc46ab0
veniq <veniq137@gmail.com>
2016-11-06 11:15:46 +0200

    admin

---
+++ VeniqsLIB/src/com/veniqs/model/BookPublisher.java
@@ -0,0 +1,47 @@
+package com.veniqs.model;
+
+public class BookPublisher {
+
+	private int id;
+	private String name;
+
+	public BookPublisher(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static boolean allPublishersSellect() {
+		//!TODO Auto generated method
+		return false;
+	}
+	
+	public static boolean addToBase(String name) {
+		//!TODO Auto generated method
+		return false;
+	}
+
+	public static boolean deleteFromBase(String name) {
+		//!TODO Auto generated method
+		return false;
+
+	}
+
+	public static boolean deleteFromBase(int id) {
+		//!TODO Auto generated method
+		return false;
+	}
+
+	public boolean showBooks() {
+		//!TODO Auto generated method
+		return false;
+	}
+
+}
--- VeniqsLIB/src/com/veniqs/view/AdminPane.java
+++ VeniqsLIB/src/com/veniqs/view/AdminPane.java
@@ -1,10 +1,17 @@
 package com.veniqs.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AdminPane extends Stage {
@@ -33,7 +40,7 @@ public class AdminPane extends Stage {
 	private SplitPane rootSplitPane;
 
 	private AdminPane() {
-		
+
 		root = new BorderPane();
 		rootSplitPane = new SplitPane();
 		topPane = new StackPane();
@@ -50,17 +57,54 @@ public class AdminPane extends Stage {
 		
 		
 		
+		// topPane
+		VBox topVBox = new VBox();
+		ComboBox whatToAddBox = new ComboBox(AdminPane.getOptions());
+		Button addButton = new Button("add");
+		whatToAddBox.setPromptText("hello");
+		addButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					String st = whatToAddBox.getSelectionModel().getSelectedItem().toString();
+					System.out.println(st);
+					if (st.equals("Publisher")) System.out.println("XDXDXD");
+				} catch (NullPointerException e) {
+					System.out.println("null");
+				}
+			}
+		});
+
+		topPane.getChildren().add(topVBox);
+		topVBox.getChildren().addAll(whatToAddBox, addButton);
 		
 		
 		
 		
+		
+		
+		
+		
+		
+		
+
 		scene = new Scene(root, 1000, 800);
 		this.setResizable(false);
 		this.setScene(scene);
 		this.setTitle("Admin pane");
 		scene.getStylesheets().add(this.getClass().getResource("/application/application.css").toExternalForm());
-		
 		this.show();
+	}
+
+	private static ObservableList<String> getOptions() {
+		ObservableList<String> options = FXCollections.observableArrayList();
+		options.add("Publisher");
+		options.add("Author");
+		options.add("Language");
+		options.add("Genre");
+		options.add("Customer");
+		return options;
 	}
 
 }
