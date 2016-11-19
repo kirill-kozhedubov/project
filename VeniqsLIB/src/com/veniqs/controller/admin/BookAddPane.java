29b285df861533266c7be5dc59e891a0fc7d672b
veniq <veniq137@gmail.com>
2016-11-19 20:13:36 +0200

    b

--- VeniqsLIB/src/com/veniqs/controller/admin/BookAddPane.java
+++ VeniqsLIB/src/com/veniqs/controller/admin/BookAddPane.java
@@ -1,8 +1,10 @@
 package com.veniqs.controller.admin;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.veniqs.controller.db.DBConnector;
@@ -15,6 +17,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class BookAddPane extends GridPane {
 
@@ -31,12 +34,12 @@ public class BookAddPane extends GridPane {
 	private TextField yearTextField;
 
 	private Label genreLabel;
-	private List<TextField> genreTextFields;
-	private Button addAnotherGenreTF;
-	
+	private List<TextField> genreTextFields = new ArrayList<>();
+	private Button addAnotherGenreTextFieldButton;
+
 	private Label authorLabel;
-	private List<TextField> authorTextFields;
-	private Button addAnotherAuthorTF;
+	private List<TextField> authorTextFields = new ArrayList<>();
+	private Button addAnotherAuthorTextFieldButton;
 
 	private Button addButton;
 
@@ -51,7 +54,7 @@ public class BookAddPane extends GridPane {
 		this.setVgap(10);
 		this.setPadding(new Insets(10, 10, 10, 10));
 
-		titleLabel = new Label("Full name");
+		titleLabel = new Label("Title");
 		titleTextField = new TextField();
 
 		languageLabel = new Label("Language");
@@ -65,39 +68,47 @@ public class BookAddPane extends GridPane {
 
 		// genre
 		genreLabel = new Label("Genre");
-		addAnotherGenreTF = new Button("Add genre");
-		this.add(genreLabel, 3, 1);
-		this.add(addAnotherGenreTF, 4, 1);
+		addAnotherGenreTextFieldButton = new Button("Add");
+		HBox genreBox = new HBox(genreLabel, addAnotherGenreTextFieldButton);
+		genreBox.setSpacing(5);
+		this.add(genreBox, 4, 1);
 
 		// author
 		authorLabel = new Label("Author");
-		addAnotherAuthorTF = new Button("Add author");
-		this.add(authorLabel, 6, 1);
-		this.add(addAnotherAuthorTF, 7, 1);
+		addAnotherAuthorTextFieldButton = new Button("Add");
+		HBox authorBox = new HBox(authorLabel, addAnotherAuthorTextFieldButton);
+		authorBox.setSpacing(5);
+		this.add(authorBox, 6, 1);
 
-		
-		
-		
 		// add book button
 		addButton = new Button("Add to database");
-		handler = new AddBookHandler(whatToAddComboBox);
-
+		handler = new AddBookHandler(whatToAddComboBox, titleTextField, languageTextField, publisherTextField, yearTextField, genreTextFields, authorTextFields);
 		addButton.setOnAction(handler);
 
 		// add things to grid
-		// this.add(child, col, row);
+	
+		// genre and author
+		AddTextFieldToFormAndListHandler authorAdd = new AddTextFieldToFormAndListHandler(this, authorTextFields, 6, 2);
+		AddTextFieldToFormAndListHandler genreAdd = new AddTextFieldToFormAndListHandler(this, genreTextFields, 4, 2);
+		
+		addAnotherAuthorTextFieldButton.setOnAction(authorAdd);
+		addAnotherGenreTextFieldButton.setOnAction(genreAdd);
+		
+		// labels
 		this.add(titleLabel, 1, 2);
 		this.add(publisherLabel, 1, 3);
 		this.add(languageLabel, 1, 4);
 		this.add(yearLabel, 1, 5);
-
+		// tf's
 		this.add(titleTextField, 2, 2);
 		this.add(publisherTextField, 2, 3);
 		this.add(languageTextField, 2, 4);
 		this.add(yearTextField, 2, 5);
-
+		// buttons
 		this.add(addButton, 2, 7);
 
+		
+		
 	}
 
 }
@@ -107,56 +118,89 @@ class AddTextFieldToFormAndListHandler implements EventHandler<ActionEvent> {
 	GridPane mainGrid;
 	List<TextField> tfList;
 	int col;
-	int startRow;
-	
-	AddTextFieldToFormAndListHandler(GridPane mainGrid, List<TextField> tfList, int col, int startRow) {
+	int row;
+
+	AddTextFieldToFormAndListHandler(GridPane mainGrid, List<TextField> tfList, int col, int row) {
 		this.mainGrid = mainGrid;
 		this.tfList = tfList;
 		this.col = col;
-		this.startRow = startRow;
-		
+		this.row = row;
+
 	}
-	
+
 	@Override
 	public void handle(ActionEvent arg0) {
-		
+		TextField tf = new TextField();
+		tf.setMaxWidth(100);
+		tf.setMinWidth(70);
+		mainGrid.add(tf, col, row);
+		row++;
+		tfList.add(tf);
 	}
+
 }
 
 class AddBookHandler implements EventHandler<ActionEvent> {
 
 	private ComboBox<String> comboBox;
+	TextField title;
+	TextField language; 
+	TextField publisher;
+	TextField year; 
+	List<TextField> genre;
+	List<TextField> author;
 
-	AddBookHandler(ComboBox<String> comboBox) {
+	AddBookHandler(ComboBox<String> comboBox, TextField title, TextField language, TextField publisher, TextField year, List<TextField> genre, List<TextField> author) {
 		this.comboBox = comboBox;
-
+		this.title = title;
+		this.language = language;
+		this.publisher = publisher;
+		this.year = year;
+		this.genre = genre;
+		this.author = author;
+		
 	}
 
 	@Override
 	public void handle(ActionEvent arg0) {
+addBook();
+	}
+	
+	
+	private int addBook() {
+		int addedOrOverridenBookID = -1;
+		String titleStr = title.getText();
+		String languageStr = language.getText();
+		String publisherStr = publisher.getText();
+		String yearStr = year.getText();
 		try {
 			DBConnector connection = new DBConnector();
 			Connection c = connection.getConnection();
-			System.out.println("Opened database successfully LibrarianAddPane");
+			System.out.println("Opened database successfully Book add pane");
 			Statement stmt = c.createStatement();
-			// String query = "SELECT create_or_get_librarian_id('" +
-			// name.getText() + "', '" + login.getText() + "', '" +
-			// password.getText() + "');";
-			// stmt.executeQuery(query);
-			/*
-			 * System.out.println(query); ResultSet rs =
-			 * stmt.executeQuery(query); while (rs.next()) { int id =
-			 * rs.getInt(rs.getMetaData().getColumnName(1));
-			 * System.out.println(id + " " + rs.getMetaData().getColumnName(1));
-			 * } rs.close();
-			 */
+			String query = "SELECT book_create_or_get_id('" + titleStr + "', " + yearStr + ", '" + languageStr + "', '"
+					+ publisherStr + "');";
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				addedOrOverridenBookID = rs.getInt(rs.getMetaData().getColumnName(1));
+				System.out.println(addedOrOverridenBookID + " " + rs.getMetaData().getColumnName(1));
+			}
+			rs.close();
+
 			stmt.close();
-			c.commit();
+			//c.commit();
 			c.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println("ОШИБКА " + e.getClass().getName() + ": " + e.getMessage());
 		}
+		
+		
+		return addedOrOverridenBookID;
 	}
+	
+	
+	
 }
